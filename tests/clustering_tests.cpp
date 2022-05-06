762542ab61f2c7659cb9c3ee99a0b7f804381f11
SoultatosStefanos <stefanoss1498@gmail.com>
2022-05-06 13:24:55 +0300

    testing the k_spanning_tree clusterer

--- tests/clustering_tests.cpp
+++ tests/clustering_tests.cpp
@@ -22,12 +22,67 @@ TEST(K_spanning_tree, Empty_yields_empty)
 {
     Graph initial;
     Graph expected = initial; // empty
-    auto k = urandom(1, 10);
+    const auto k = urandom(1, 10);
 
     Clustering::k_spanning_tree(
         initial, k, [](const auto& g) { return g; }, boost::get(&Edge::weight, initial));
 
     ASSERT_TRUE(boost::isomorphism(initial, expected));
+}
+
+TEST(K_spanning_tree, Clustering_given_mst)
+{
+    constexpr auto k = 3;
+
+    Graph g;
+
+    auto v1 = boost::add_vertex({1}, g);
+    auto v2 = boost::add_vertex({2}, g);
+    auto v3 = boost::add_vertex({3}, g);
+    auto v4 = boost::add_vertex({4}, g);
+    auto v5 = boost::add_vertex({5}, g);
+
+    boost::add_edge(v1, v2, {7}, g);
+    boost::add_edge(v1, v3, {2}, g);
+    boost::add_edge(v1, v4, {4}, g);
+    boost::add_edge(v2, v3, {3}, g);
+    boost::add_edge(v2, v4, {5}, g);
+    boost::add_edge(v3, v4, {2}, g);
+    boost::add_edge(v3, v5, {6}, g);
+    boost::add_edge(v4, v5, {4}, g);
+
+    Graph expected;
+
+    auto vv1 = boost::add_vertex({1}, expected);
+    boost::add_vertex({2}, expected);
+    auto vv3 = boost::add_vertex({3}, expected);
+    auto vv4 = boost::add_vertex({4}, expected);
+    boost::add_vertex({5}, expected);
+
+    boost::add_edge(vv1, vv3, {2}, expected);
+    boost::add_edge(vv3, vv4, {2}, expected);
+
+    Clustering::k_spanning_tree(
+        g, k,
+        [](const auto&) {
+            Graph mst;
+
+            auto v1 = boost::add_vertex({1}, mst);
+            auto v2 = boost::add_vertex({2}, mst);
+            auto v3 = boost::add_vertex({3}, mst);
+            auto v4 = boost::add_vertex({4}, mst);
+            auto v5 = boost::add_vertex({5}, mst);
+
+            boost::add_edge(v1, v3, {2}, mst);
+            boost::add_edge(v2, v3, {3}, mst);
+            boost::add_edge(v3, v4, {2}, mst);
+            boost::add_edge(v4, v5, {4}, mst);
+
+            return mst;
+        },
+        boost::get(&Edge::weight, g));
+
+    ASSERT_TRUE(boost::isomorphism(g, expected));
 }
 
 } // namespace GV::Clustering::Tests
