0946d0d945d7d6d84c68561a3029e4970457d11f
SoultatosStefanos <stefanoss1498@gmail.com>
2022-05-14 18:06:17 +0300

    Fixing th hcc implementation

--- include/graph_visualizer/clustering/highly_connected_components_clustering.hpp
+++ include/graph_visualizer/clustering/highly_connected_components_clustering.hpp
@@ -10,37 +10,77 @@
 #include <boost/graph/isomorphism.hpp>
 #include <cassert>
 #include <concepts>
+#include <set>
 #include <type_traits>
 #include <utility>
 
 namespace GV::Clustering {
 
-// Generic Highly Connected Componentes Clustering algorithm
-// O(MinimumCut + EdgeConnectivity + V + E)
-template <typename MutableGraph, typename MinimumCut, typename EdgeConnectivity>
-void highly_connected_components_clustering(MutableGraph& g, MinimumCut min_cut,
-                                            EdgeConnectivity edge_connectivity)
+namespace Details {
+
+template <typename Graph, typename ParityMap>
+requires std::totally_ordered<typename boost::graph_traits<Graph>::edge_descriptor>
+auto min_cut_edge_set(const Graph& g, ParityMap parity)
 {
-    using Partition = std::pair<MutableGraph, MutableGraph>;
-    using EdgesNum = typename boost::graph_traits<MutableGraph>::edges_size_type;
+    using Edge = typename boost::graph_traits<Graph>::edge_descriptor;
+    using MinCutEdgeSet = std::set<Edge>;
+
+    MinCutEdgeSet edges;
+
+    const auto& [begin, end] = boost::edges(g);
+    for (auto iter = begin; iter != end; ++iter) {
+        const auto src = boost::source(*iter, g);
+        const auto trgt = boost::target(*iter, g);
+
+        if (boost::get(parity, src) != boost::get(parity, trgt)) // different parity
+            edges.insert(boost::edge(src, trgt));
+    }
+
+    return edges;
+}
+
+template <typename Graph, typename ParityMap>
+auto divide(const Graph& g, ParityMap parity,
+            const std::set<typename boost::graph_traits<Graph>::edge_descriptor>& min_cut_edges)
+{
+    using Partition = std::pair<Graph, Graph>;
+}
+
+} // namespace Details
+
+// Generic Highly Connected Componentes Clustering algorithm
+template <typename MutableGraph, typename MinimumCut, typename ParityMap>
+requires std::totally_ordered<typename boost::graph_traits<MutableGraph>::edge_descriptor>
+void highly_connected_components_clustering(MutableGraph& g, MinimumCut min_cut,
+                                            ParityMap parity = boost::dummy_property_map)
+{
+    using Vertex = typename boost::graph_traits<MutableGraph>::vertex_descriptor;
+    using ParityKey = typename boost::property_traits<ParityMap>::key_type;
+    using ParityValue = typename boost::property_traits<ParityMap>::value_type;
 
     BOOST_CONCEPT_ASSERT((boost::GraphConcept<MutableGraph>) );
+    BOOST_CONCEPT_ASSERT((boost::ReadWritePropertyMapConcept<ParityMap, Vertex>) );
 
     static_assert(std::is_trivially_copyable_v<MinimumCut>);
-    static_assert(std::is_trivially_copyable_v<EdgeConnectivity>);
-    static_assert(std::is_invocable_r_v<Partition, MinimumCut, MutableGraph>);
-    static_assert(std::is_invocable_r_v<EdgesNum, EdgeConnectivity, MutableGraph, Partition>);
+    static_assert(std::is_trivially_copyable_v<ParityMap>);
+    static_assert(std::is_same_v<ParityKey, Vertex>);
+    static_assert(std::is_same_v<ParityValue, bool>);
+    static_assert(std::is_invocable_v<MinimumCut, MutableGraph, ParityMap>);
 
-    auto partition = min_cut(g);
+    min_cut(g, parity); // fill parity map
 
-    assert(boost::isomorphism(g, merge(partition.first, partition.second)) && "invalid cut");
+    const auto min_cut_edges = Details::min_cut_edge_set(g, parity);
 
-    if (edge_connectivity(g, partition) > (boost::num_vertices(g) / 2)) return; // break recursion
+    const auto edge_connectivity = min_cut_edges.size();
+    if (edge_connectivity > (boost::num_vertices(g) / 2)) return; // break recursion
 
-    highly_connected_components_clustering_impl(partition.first, min_cut, edge_connectivity);
-    highly_connected_components_clustering_impl(partition.second, min_cut, edge_connectivity);
+    auto partition = Details::divide(g, parity, min_cut_edges);
+    assert(!boost::isomorphism(g, merge(partition.first, partition.second)) && "invalid cut");
 
-    g = merge(partition.first, partition.second);
+    highly_connected_components_clustering_impl(partition.first, min_cut, parity, cmp);
+    highly_connected_components_clustering_impl(partition.second, min_cut, parity, cmp);
+
+    g = merge(partition.first, partition.second); // merge partitions into a cut single graph
 }
 
 } // namespace GV::Clustering
---
+++ include/graph_visualizer/clustering/minimum_cut.hpp
@@ -0,0 +1,11 @@
+// Contains generic Minimum Cut algorithms and boost adaptors
+// Soultatos Stefanos 2022
+
+#pragma once
+
+#include <algorithm>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/stoer_wagner_min_cut.hpp>
+#include <concepts>
+
+namespace GV::Clustering {}
