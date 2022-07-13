e74926a668c3ca51290f373e41bca1d08ef22a9e
SoultatosStefanos <stefanoss1498@gmail.com>
2022-07-13 17:20:46 +0300

    Set up the update_layout_service_tests

--- tests/visualization/layout/services_tests.cpp
+++ tests/visualization/layout/services_tests.cpp
@@ -12,8 +12,9 @@ using namespace visualization::layout;
 using namespace visualization::command;
 using namespace visualization::communication;
 using namespace utility;
+using namespace testing;
 
-class Given_a_bus_graph_cmds_topology_and_layout : public testing::Test
+class Given_a_bus_graph_cmds_topology_and_layout : public Test
 {
 public:
     void SetUp() override
@@ -37,6 +38,9 @@ class An_update_layout_service
     : public Given_a_bus_graph_cmds_topology_and_layout
 {
 public:
+    using mock_subscriber =
+        NiceMock<MockFunction<event_bus::subscriber<layout_request_event>>>;
+
     void SetUp() override
     {
         Given_a_bus_graph_cmds_topology_and_layout::SetUp();
@@ -56,6 +60,45 @@ TEST_F(An_update_layout_service,
     std::invoke(*service, layout_request_event("gursoy_atun", "gursoy_atun"));
 
     ASSERT_EQ(prev, &l);
+}
+
+TEST_F(An_update_layout_service,
+       Wont_post_a_layout_response_when_requested_same_type)
+{
+    const auto event = layout_request_event("gursoy_atun", "gursoy_atun");
+    mock_subscriber mock;
+    bus->subscribe<layout_request_event>(mock.AsStdFunction());
+
+    EXPECT_CALL(mock, Call(event)).Times(0);
+
+    std::invoke(*service, event);
+}
+
+// TODO
+TEST_F(An_update_layout_service,
+       Will_change_the_layout_when_requested_different_type)
+{
+    SUCCEED();
+}
+
+// TODO
+TEST_F(An_update_layout_service,
+       Will_post_a_layout_response_when_requested_different_type)
+{
+    SUCCEED();
+}
+
+// TODO
+TEST_F(An_update_layout_service,
+       Will_revert_to_initial_layout_after_execute_and_undo)
+{
+    SUCCEED();
+}
+
+TEST_F(An_update_layout_service,
+       Will_revert_to_changed_layout_after_execute_undo_and_redo)
+{
+    SUCCEED();
 }
 
 } // namespace
