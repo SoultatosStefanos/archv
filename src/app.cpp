0f0e677a9d79a8c577c821b3d3e546d15806210c
SoultatosStefanos <stefanoss1498@gmail.com>
2022-07-30 14:33:17 +0300

    app refactoring

--- src/app.cpp
+++ src/app.cpp
@@ -1,121 +1,206 @@
 #include "app.hpp"
 
-#include <OGRE/OgreManualObject.h>
+#include <OGRE/Bites/OgreCameraMan.h>
+#include <OGRE/Ogre.h>
+#include <cassert>
+#include <memory>
 
 using namespace Ogre;
 using namespace OgreBites;
-using namespace features;
 
-app::app(const graph& g) : ApplicationContext("Architecture Visualizer"), m_g{g}
-{}
+/***********************************************************
+ * Setup                                                   *
+ ***********************************************************/
 
-// TODO Cleanup
+app::app(const dependencies::graph& g) : ApplicationContext("ARCHV"), m_g{g} {}
 
-// FIXME
 void app::setup()
 {
     ApplicationContext::setup();
-    addInputListener(this);
+    setup_scene();
+    setup_lighting();
+    setup_camera();
+    setup_resources();
+    setup_entities();
+    setup_command_history();
+    setup_layout();
+    setup_input();
+}
 
-    // scene manager
+void app::setup_scene()
+{
     m_scene = getRoot()->createSceneManager();
-    RTShader::ShaderGenerator::getSingleton().addSceneManager(m_scene);
-    m_scene->setAmbientLight(ColourValue(0.5, 0.5, 0.5));
+    assert(m_scene);
 
-    // light
-    auto* light = m_scene->createLight();
-    auto* light_node =
-        m_scene->getRootSceneNode()->createChildSceneNode("LightNode");
-    light_node->attachObject(light);
-    light_node->setPosition(20, 80, 50);
+    RTShader::ShaderGenerator::getSingletonPtr()->addSceneManager(m_scene);
+}
 
-    // camera
-    auto* cam_node = m_scene->getRootSceneNode()->createChildSceneNode("Cam");
-    auto* cam = m_scene->createCamera("C");
-    cam->setNearClipDistance(5); // specific to this sample
-    cam->setAutoAspectRatio(true);
-    cam_node->attachObject(cam);
-    cam_node->setPosition(0, 0, 140);
-    getRenderWindow()->addViewport(cam);
+void app::setup_lighting()
+{
+    m_scene->setAmbientLight(ColourValue(0.5, 0.5, 0.5)); // TODO Config
 
-    // nodes
+    m_light = m_scene->createLight();
+    assert(m_light);
+
+    m_light_node = m_scene->getRootSceneNode()->createChildSceneNode();
+    assert(m_light_node);
+    m_light_node->attachObject(m_light);
+    m_light_node->setPosition(20, 80, 50); // TODO Config
+}
+
+void app::setup_camera()
+{
+    m_cam = m_scene->createCamera("camera");
+    assert(m_cam);
+    m_cam->setNearClipDistance(5);   // TODO Config
+    m_cam->setAutoAspectRatio(true); // TODO Config
+
+    m_cam_node = m_scene->getRootSceneNode()->createChildSceneNode();
+    assert(m_cam_node);
+    m_cam_node->attachObject(m_cam);
+    m_cam_node->setPosition(0, 0, 140); // TODO Config
+
+    getRenderWindow()->addViewport(m_cam);
+}
+
+void app::setup_resources()
+{
+    ResourceGroupManager::getSingleton().initialiseAllResourceGroups();
+}
+
+void app::setup_entities()
+{
     for (auto v : boost::make_iterator_range(boost::vertices(m_g)))
     {
-        auto* entity = m_scene->createEntity("ogrehead.mesh");
+        auto* entity = m_scene->createEntity("ogrehead.mesh"); // TODO Config
         auto* node = m_scene->getRootSceneNode()->createChildSceneNode(m_g[v]);
         node->attachObject(entity);
-        node->setScale(0.15, 0.15, 0.15);
+        node->setScale(0.15, 0.15, 0.15); // TODO Config
     }
-
-    // cameraman
-    m_cameraman = new CameraMan(cam_node);
-    addInputListener(m_cameraman);
-
-    initialize_layout();
 }
 
-void app::initialize_layout()
+void app::setup_command_history()
 {
-    features::layout::core::get().initialize(
-        m_cmds,
-        m_g,
-        dependencies::weight_map([](auto) { return 1; }), // FIXME
-        "Gursoy Atun",
-        "Sphere",
-        100);
-
-    features::layout::core::get().connect(
-        [this](const features::layout::layout& l, const auto&) {
-            for (auto v : boost::make_iterator_range(boost::vertices(m_g)))
-                m_scene->getSceneNode(m_g[v])->setPosition(
-                    l.x(v), l.y(v), l.z(v));
-        });
+    m_cmds = std::make_unique<utility::command_history>();
 }
 
-// FIXME
+void app::setup_layout()
+{
+    m_layout_sys = std::make_unique<features::layout::core>(
+        *m_cmds,
+        m_g,
+        dependencies::weight_map([](auto) { return 1; }),
+        "Gursoy Atun", // TODO Config
+        "Sphere",      // TODO Config
+        100);          // TODO Config
+
+    const auto draw_nodes = [this](const features::layout::layout& l) {
+        for (auto v : boost::make_iterator_range(boost::vertices(m_g)))
+            m_scene->getSceneNode(m_g[v])->setPosition(l.x(v), l.y(v), l.z(v));
+    };
+
+    m_layout_sys->connect(
+        [draw_nodes](const auto& l, const auto&) { draw_nodes(l); });
+
+    draw_nodes(m_layout_sys->get_layout());
+}
+
+void app::setup_input()
+{
+    m_cameraman = std::make_unique<CameraMan>(m_cam_node);
+
+    addInputListener(m_cameraman.get());
+    addInputListener(this);
+}
+
+/***********************************************************
+ * Shutdown                                                *
+ ***********************************************************/
+
 void app::shutdown()
 {
-    // cameraman
-    removeInputListener(m_cameraman);
-    delete m_cameraman;
-
-    // nodes
-    // TODO remove and destroy nodes from root scene
-    m_scene->destroyAllEntities();
-
-    // camera
-    m_scene->getRootSceneNode()->removeAndDestroyChild("Cam");
-    m_scene->destroyAllCameras();
-
-    // light
-    m_scene->getRootSceneNode()->removeAndDestroyChild("LightNode");
-    m_scene->destroyAllLights();
-
-    // scene manager
-    RTShader::ShaderGenerator::getSingleton().removeSceneManager(m_scene);
-    RTShader::ShaderGenerator::getSingleton().destroy();
-    getRoot()->destroySceneManager(m_scene);
-
+    shutdown_input();
+    shutdown_layout();
+    shutdown_command_history();
+    shutdown_entities();
+    shutdown_resources();
+    shutdown_camera();
+    shutdown_lighting();
+    shutdown_scene();
     ApplicationContext::shutdown();
 }
 
+void app::shutdown_input()
+{
+    removeInputListener(this);
+    removeInputListener(m_cameraman.get());
+
+    m_cameraman.reset();
+}
+
+void app::shutdown_layout()
+{
+    m_layout_sys.reset();
+}
+
+void app::shutdown_command_history()
+{
+    m_cmds.reset();
+}
+
+void app::shutdown_entities()
+{
+    for (auto v : boost::make_iterator_range(boost::vertices(m_g)))
+        m_scene->getRootSceneNode()->removeAndDestroyChild(m_g[v]);
+
+    m_scene->destroyEntity("ogrehead.mesh"); // TODO Config
+}
+
+void app::shutdown_resources()
+{
+    ResourceGroupManager::getSingleton().shutdownAll();
+}
+
+void app::shutdown_camera()
+{
+    getRenderWindow()->removeViewport(0);
+
+    m_scene->getRootSceneNode()->removeAndDestroyChild(m_cam_node);
+    m_scene->destroyCamera(m_cam);
+}
+
+void app::shutdown_lighting()
+{
+    m_scene->getRootSceneNode()->removeAndDestroyChild(m_light_node);
+    m_scene->destroyLight(m_light);
+}
+
+void app::shutdown_scene()
+{
+    RTShader::ShaderGenerator::getSingletonPtr()->removeSceneManager(m_scene);
+    getRoot()->destroySceneManager(m_scene);
+}
+
+/***********************************************************
+ * Input                                                   *
+ ***********************************************************/
+
+// TODO Config
 auto app::keyPressed(const OgreBites::KeyboardEvent& e) -> bool
 {
-    static constexpr auto quit_sym = SDLK_ESCAPE;
-
-    if (e.keysym.sym == quit_sym)
+    if (e.keysym.sym == SDLK_ESCAPE)
         getRoot()->queueEndRendering();
-#if (1) // FIXME
+#if (0) // FIXME
     else if (e.keysym.sym == SDLK_LEFT)
-        m_cmds.undo();
+        m_cmds->undo();
     else if (e.keysym.sym == SDLK_RIGHT)
-        m_cmds.redo();
+        m_cmds->redo();
     else if (e.keysym.sym == 'o')
-        layout::core::get().update_topology("Sphere", 80);
+        m_layout_sys->update_topology("Sphere", 80);
     else if (e.keysym.sym == 'p')
-        layout::core::get().update_topology("Cube", 80);
+        m_layout_sys->update_topology("Cube", 80);
     else if (e.keysym.sym == 'r')
-        layout::core::get().revert_to_defaults();
+        m_layout_sys->revert_to_defaults();
 #endif
 
     return true;
--- src/features/layout/core.cpp
+++ src/features/layout/core.cpp
@@ -3,84 +3,64 @@
 namespace features::layout
 {
 
-void core::initialize(command_history& cmds,
-                      const graph& g,
-                      weight_map edge_weight,
-                      const std::string& layout_type,
-                      const std::string& topolgy_type,
-                      double topology_scale)
+core::core(command_history& cmds,
+           const graph& g,
+           weight_map edge_weight,
+           const std::string& layout_type,
+           const std::string& topolgy_type,
+           double topology_scale)
+    : m_topology{topology_factory::make_topology(topolgy_type, topology_scale)},
+      m_layout{layout_factory::make_layout(
+          layout_type, g, *m_topology, edge_weight)},
+      m_update_layout{cmds, g, edge_weight, m_layout, m_topology},
+      m_update_topology{cmds, g, edge_weight, m_layout, m_topology},
+      m_revert_to_defaults{cmds,
+                           g,
+                           edge_weight,
+                           layout_type,
+                           topolgy_type,
+                           topology_scale,
+                           m_layout,
+                           m_topology}
 {
-    m_topology = topology_factory::make_topology(topolgy_type, topology_scale);
-    m_layout =
-        layout_factory::make_layout(layout_type, g, *m_topology, edge_weight);
-
-    m_update_layout = std::make_unique<update_layout_service>(
-        cmds, g, edge_weight, m_layout, m_topology);
-
-    m_update_topology = std::make_unique<update_topology_service>(
-        cmds, g, edge_weight, m_layout, m_topology);
-
-    m_revert_to_defaults =
-        std::make_unique<revert_to_defaults_service>(cmds,
-                                                     g,
-                                                     edge_weight,
-                                                     layout_type,
-                                                     topolgy_type,
-                                                     topology_scale,
-                                                     m_layout,
-                                                     m_topology);
-
-    m_update_layout->connect(
+    m_update_layout.connect(
         [this](const auto& l, const auto& s) { m_signal(l, s); });
-    m_update_topology->connect(
+    m_update_topology.connect(
         [this](const auto& l, const auto& s) { m_signal(l, s); });
-    m_revert_to_defaults->connect(
+    m_revert_to_defaults.connect(
         [this](const auto& l, const auto& s) { m_signal(l, s); });
 }
 
-void core::reset()
-{
-    m_topology.reset();
-    m_layout.reset();
-    m_update_layout.reset();
-    m_update_topology.reset();
-    m_revert_to_defaults.reset();
-}
-
-auto core::get_layout_type() const -> std::string
+auto core::get_layout() const -> const layout&
 {
     assert(m_layout);
-    return m_layout->desc();
+    return *m_layout;
 }
 
-auto core::get_topology_type() const -> std::string
+auto core::get_topology() const -> const topology&
 {
     assert(m_topology);
-    return m_topology->desc();
-}
-
-auto core::get_topology_scale() const -> double
-{
-    assert(m_topology);
-    return m_topology->scale();
+    return *m_topology;
 }
 
 void core::update_layout(const std::string& type)
 {
-    assert(m_update_layout);
-    m_update_layout->execute(type);
+    m_update_layout.execute(type);
 }
 
 void core::update_topology(const std::string& type, double scale)
 {
-    assert(m_update_topology);
-    m_update_topology->execute(type, scale);
+    m_update_topology.execute(type, scale);
 }
 
 void core::revert_to_defaults()
 {
-    assert(m_revert_to_defaults);
-    m_revert_to_defaults->execute();
+    m_revert_to_defaults.execute();
+}
+
+auto core::connect(const slot_type& slot) -> connection
+{
+    return m_signal.connect(slot);
 }
 
 } // namespace features::layout
