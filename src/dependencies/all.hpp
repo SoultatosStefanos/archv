7415dd260db38c42e0551bb3f7f12b38eaca354f
SoultatosStefanos <stefanoss1498@gmail.com>
2022-07-31 23:23:06 +0300

    dependencies/all.hpp

---
+++ src/dependencies/all.hpp
@@ -0,0 +1,10 @@
+// Distributes all headers from this directory.
+// Soultatos Stefanos 2022
+
+#ifndef DEPENDENCIES_ALL_HPP
+#define DEPENDENCIES_ALL_HPP
+
+#include "core.hpp"
+#include "weight_repo.hpp"
+
+#endif // DEPENDENCIES_ALL_HPP
