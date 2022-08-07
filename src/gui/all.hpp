a2d6352dc3e39488412ae137c22a3fed0fb98ba8
SoultatosStefanos <stefanoss1498@gmail.com>
2022-08-08 01:36:18 +0300

    Refactored the paused state

---
+++ src/gui/all.hpp
@@ -0,0 +1,12 @@
+// Distributes all headers from this directory.
+// Soultatos Stefanos 2022
+
+#ifndef GUI_ALL_HPP
+#define GUI_ALL_HPP
+
+#include "mygui_input_dispatcher.hpp"
+#include "mygui_manager.hpp"
+#include "pause_menu.hpp"
+#include "ui_manager.hpp"
+
+#endif // GUI_ALL_HPP
---
+++ src/gui/allfwd.hpp
@@ -0,0 +1,17 @@
+// Distributes all types from this directory fwd declared.
+// Soultatos Stefanos 2022
+
+#ifndef GUI_ALLFWD_HPP
+#define GUI_ALLFWD_HPP
+
+namespace gui
+{
+
+class mygui_input_dispatcher;
+class mygui_manager;
+class pause_menu;
+class ui_manager;
+
+} // namespace gui
+
+#endif // GUI_ALLFWD_HPP
