2c317d2e3af73cfdbe64f4f7c5d2af5cd1f4359b
SoultatosStefanos <stefanoss1498@gmail.com>
2022-09-08 14:02:17 +0300

    ui package

---
+++ src/ui/allfwd.hpp
@@ -0,0 +1,11 @@
+// Distributes all types from this directory fwd declared.
+// Soultatos Stefanos 2022
+
+#ifndef UI_ALLFWD_HPP
+#define UI_ALLFWD_HPP
+
+namespace ui
+{
+}
+
+#endif // UI_ALLFWD_HPP
