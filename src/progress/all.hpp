2c8115cdc08e19ca3b30dbac26872b5f88c913e1
SoultatosStefanos <stefanoss1498@gmail.com>
2022-10-22 20:42:50 +0300

    cleaning up

--- src/progress/all.hpp
+++
@@ -1,9 +0,0 @@
-// Distributes all headers from this directory.
-// Soultatos Stefanos 2022
-
-#ifndef PROGRESS_ALL_HPP
-#define PROGRESS_ALL_HPP
-
-#include "task.hpp"
-
-#endif // PROGRESS_ALL_HPP
