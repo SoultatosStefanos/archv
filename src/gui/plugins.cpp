a7e01b469dab3e9ca97d9c03208e6cedb2d470c8
SoultatosStefanos <stefanoss1498@gmail.com>
2022-11-02 19:09:33 +0200

    extended the gui pluggins

--- src/gui/plugins.cpp
+++ src/gui/plugins.cpp
@@ -11,7 +11,8 @@ namespace
     layout_set plugged_layouts;
     topology_set plugged_topologies;
     factor_set plugged_factors;
-
+    clusterer_set plugged_clusterers;
+    mst_finder_set plugged_mst_finders;
 } // namespace
 
 auto dependencies() -> const dependency_set&
@@ -34,6 +35,16 @@ auto factors() -> const factor_set&
     return plugged_factors;
 }
 
+auto clusterers() -> const clusterer_set&
+{
+    return plugged_clusterers;
+}
+
+auto mst_finders() -> const mst_finder_set&
+{
+    return plugged_mst_finders;
+}
+
 auto install_dependencies(dependency_set set) -> void
 {
     plugged_dependencies = std::move(set);
@@ -52,6 +63,16 @@ auto install_topologies(topology_set set) -> void
 auto install_factors(factor_set set) -> void
 {
     plugged_factors = std::move(set);
+}
+
+auto install_clusterers(clusterer_set set) -> void
+{
+    plugged_clusterers = std::move(set);
+}
+
+auto install_mst_finders(mst_finder_set set) -> void
+{
+    plugged_mst_finders = std::move(set);
 }
 
 } // namespace gui::plugins
--- src/gui/plugins.hpp
+++ src/gui/plugins.hpp
@@ -19,11 +19,15 @@ using weight = int;
 using layout = std::string_view;
 using topology = std::string_view;
 using factor = std::string_view;
+using clusterer = std::string_view;
+using mst_finder = std::string_view;
 
 using dependency_set = std::unordered_set< dependency >;
 using layout_set = std::unordered_set< layout >;
 using topology_set = std::unordered_set< topology >;
 using factor_set = std::unordered_set< factor >;
+using clusterer_set = std::unordered_set< clusterer >;
+using mst_finder_set = std::unordered_set< mst_finder >;
 
 /***********************************************************
  * Observers                                               *
@@ -33,6 +37,8 @@ auto dependencies() -> const dependency_set&;
 auto layouts() -> const layout_set&;
 auto topologies() -> const topology_set&;
 auto factors() -> const factor_set&;
+auto clusterers() -> const clusterer_set&;
+auto mst_finders() -> const mst_finder_set&;
 
 /***********************************************************
  * Installers                                              *
@@ -42,6 +48,8 @@ auto install_dependencies(dependency_set set) -> void;
 auto install_layouts(layout_set set) -> void;
 auto install_topologies(topology_set set) -> void;
 auto install_factors(factor_set set) -> void;
+auto install_clusterers(clusterer_set set) -> void;
+auto install_mst_finders(mst_finder_set set) -> void;
 
 } // namespace gui::plugins
 
