dd22e15b6e358f042c2d0ada8c023e4aa7d2c337
SoultatosStefanos <stefanoss1498@gmail.com>
2022-11-19 21:11:53 +0200

    implementing a presentation layer

---
+++ src/presentation/commands.hpp
@@ -0,0 +1,207 @@
+// Contains all of the presentation level commands.
+// Soultatos Stefanos 2022
+
+#ifndef PRESENTATION_COMMANDS_HPP
+#define PRESENTATION_COMMANDS_HPP
+
+#include "typedefs.hpp"
+
+namespace presentation
+{
+
+/***********************************************************
+ * Weights                                                 *
+ ***********************************************************/
+
+auto update_dependency_weight(
+    command_history& cmds,
+    weights_backend& backend,
+    weights_backend::dependency_type dependency,
+    weights_backend::weight_type weight) -> void;
+
+auto restore_weights(command_history& cmds, weights_backend& backend) -> void;
+
+/***********************************************************
+ * Layout                                                  *
+ ***********************************************************/
+
+auto update_layout(
+    command_history& cmds,
+    layout_backend& backend,
+    layout_backend::id_type layout_id) -> void;
+
+auto update_layout_topology(
+    command_history& cmds,
+    layout_backend& backend,
+    layout_backend::id_type topology_id) -> void;
+
+auto update_layout_scale(
+    command_history& cmds,
+    layout_backend& backend,
+    layout_backend::scale_type scale) -> void;
+
+auto restore_layout(command_history& cmds, layout_backend& backend) -> void;
+
+/***********************************************************
+ * Scaling                                                 *
+ ***********************************************************/
+
+auto update_scaling_factor_dims(
+    command_history& cmds,
+    scaling_backend& b,
+    scaling_backend::tag_type tag,
+    scaling_backend::dims_type dims) -> void;
+
+auto update_scaling_factor_baseline(
+    command_history& cmds,
+    scaling_backend& b,
+    scaling_backend::tag_type tag,
+    scaling_backend::baseline_type baseline) -> void;
+
+auto update_scaling_factor_enablement(
+    command_history& cmds,
+    scaling_backend& b,
+    scaling_backend::tag_type tag,
+    scaling_backend::enabled_type enabled) -> void;
+
+auto update_scaling_factor_min_ratio(
+    command_history& cmds,
+    scaling_backend& b,
+    scaling_backend::tag_type tag,
+    scaling_backend::ratio_type ratio) -> void;
+
+auto update_scaling_factor_max_ratio(
+    command_history& cmds,
+    scaling_backend& b,
+    scaling_backend::tag_type tag,
+    scaling_backend::ratio_type ratio) -> void;
+
+auto restore_scaling(command_history& cmds, scaling_backend& b) -> void;
+
+/***********************************************************
+ * Degrees                                                 *
+ ***********************************************************/
+
+auto update_in_degree_evaluation_light_threshold(
+    command_history& cmds,
+    degrees_backend& backend,
+    degrees_backend::threshold_type thres) -> void;
+
+auto update_out_degree_evaluation_light_threshold(
+    command_history& cmds,
+    degrees_backend& backend,
+    degrees_backend::threshold_type thres) -> void;
+
+auto update_in_degree_evaluation_medium_threshold(
+    command_history& cmds,
+    degrees_backend& backend,
+    degrees_backend::threshold_type thres) -> void;
+
+auto update_out_degree_evaluation_medium_threshold(
+    command_history& cmds,
+    degrees_backend& backend,
+    degrees_backend::threshold_type thres) -> void;
+
+auto update_in_degree_evaluation_heavy_threshold(
+    command_history& cmds,
+    degrees_backend& backend,
+    degrees_backend::threshold_type thres) -> void;
+
+auto update_out_degree_evaluation_heavy_threshold(
+    command_history& cmds,
+    degrees_backend& backend,
+    degrees_backend::threshold_type thres) -> void;
+
+auto update_in_degree_evaluation_light_particles(
+    command_history& cmds,
+    degrees_backend& backend,
+    degrees_backend::particle_system_type particles) -> void;
+
+auto update_out_degree_evaluation_light_particles(
+    command_history& cmds,
+    degrees_backend& backend,
+    degrees_backend::particle_system_type particles) -> void;
+
+auto update_in_degree_evaluation_medium_particles(
+    command_history& cmds,
+    degrees_backend& backend,
+    degrees_backend::particle_system_type particles) -> void;
+
+auto update_out_degree_evaluation_medium_particles(
+    command_history& cmds,
+    degrees_backend& backend,
+    degrees_backend::particle_system_type particles) -> void;
+
+auto update_in_degree_evaluation_heavy_particles(
+    command_history& cmds,
+    degrees_backend& backend,
+    degrees_backend::particle_system_type particles) -> void;
+
+auto update_out_degree_evaluation_heavy_particles(
+    command_history& cmds,
+    degrees_backend& backend,
+    degrees_backend::particle_system_type particles) -> void;
+
+auto update_in_degree_evaluation_applied(
+    command_history& cmds,
+    degrees_backend& backend,
+    degrees_backend::applied_type applied) -> void;
+
+auto update_out_degree_evaluation_applied(
+    command_history& cmds,
+    degrees_backend& backend,
+    degrees_backend::applied_type applied) -> void;
+
+auto restore_in_degree_evaluation(
+    command_history& cmds, degrees_backend& backend) -> void;
+
+auto restore_out_degree_evaluation(
+    command_history& cmds, degrees_backend& backend) -> void;
+
+auto restore_degrees(command_history& cmds, degrees_backend& backend) -> void;
+
+/***********************************************************
+ * Clustering                                              *
+ ***********************************************************/
+
+auto update_clusterer(
+    command_history& cmds,
+    clustering_backend& backend,
+    clustering_backend::id_type id) -> void;
+
+auto update_clustering_mst_finder(
+    command_history& cmds,
+    clustering_backend& backend,
+    clustering_backend::id_type id) -> void;
+
+auto update_clustering_k(
+    command_history& cmds,
+    clustering_backend& backend,
+    clustering_backend::k_type k) -> void;
+
+auto update_clustering_snn_threshold(
+    command_history& cmds,
+    clustering_backend& backend,
+    clustering_backend::snn_threshold_type t) -> void;
+
+auto update_clustering_min_modularity(
+    command_history& cmds,
+    clustering_backend& backend,
+    clustering_backend::modularity_type q) -> void;
+
+auto update_clustering_llp_gamma(
+    command_history& cmds,
+    clustering_backend& backend,
+    clustering_backend::gamma_type gamma) -> void;
+
+auto update_clustering_llp_steps(
+    command_history& cmds,
+    clustering_backend& backend,
+    clustering_backend::steps_type steps) -> void;
+
+auto restore_clustering(command_history& cmds, clustering_backend& backend)
+    -> void;
+
+} // namespace presentation
+
+#endif // PRESENTATION_COMMANDS_HPP
---
+++ src/presentation/typedefs.hpp
@@ -0,0 +1,70 @@
+// Contains some presentation level typedefs.
+// Soultatos Stefanos 2022
+
+#ifndef PRESENTATION_TYPEDEFS_HPP
+#define PRESENTATION_TYPEDEFS_HPP
+
+#include "architecture/graph_interface.hpp"
+#include "clustering/all.hpp"
+#include "gui/all.hpp"
+#include "input/all.hpp"
+#include "layout/all.hpp"
+#include "rendering/all.hpp"
+#include "scaling/all.hpp"
+#include "undo_redo/all.hpp"
+#include "weights/all.hpp"
+
+namespace presentation
+{
+
+/***********************************************************
+ * Entities                                                *
+ ***********************************************************/
+
+using graph_interface = architecture::graph_interface;
+using command_history = undo_redo::command_history;
+
+/***********************************************************
+ * Backends                                                *
+ ***********************************************************/
+
+using weights_backend = graph_interface::weights_backend_type;
+using layout_backend = graph_interface::layout_backend_type;
+using scaling_backend = graph_interface::scaling_backend_type;
+using degrees_backend = rendering::degrees_ranked_backend;
+using clustering_backend = graph_interface::clustering_backend_type;
+
+/***********************************************************
+ * Renderers                                               *
+ ***********************************************************/
+
+using background_renderer = rendering::background_renderer;
+
+using graph_renderer = rendering::graph_renderer<
+    architecture::graph,
+    architecture::id_map,
+    architecture::dependency_map >;
+
+using graph_collision_checker = rendering::graph_collision_checker<
+    architecture::graph,
+    architecture::id_map,
+    architecture::dependency_map >;
+
+/***********************************************************
+ * GUI                                                     *
+ ***********************************************************/
+
+using menu_bar = gui::menu_bar;
+using weights_editor = gui::weights_editor;
+using layout_editor = gui::layout_editor;
+using scaling_editor = gui::scaling_editor;
+using degrees_editor = gui::degrees_editor;
+using clustering_editor = gui::clustering_editor;
+using controls_hud = gui::controls_hud;
+using frames_hud = gui::frames_hud;
+using overlay_manager = gui::overlay_manager;
+using popup_holder = gui::popup_holder;
+
+} // namespace presentation
+
+#endif // PRESENTATION_TYPEDEFS_HPP
