8c4a45447638f5d69c44addfee5c621fc41c1efc
SoultatosStefanos <stefanoss1498@gmail.com>
2022-11-19 21:18:51 +0200

    presentation layer all/allfwd

---
+++ src/presentation/all.hpp
@@ -0,0 +1,10 @@
+// Distributes all headers from this directory.
+// Soultatos Stefanos 2022
+
+#ifndef PRESENTATION_ALL_HPP
+#define PRESENTATION_ALL_HPP
+
+#include "commands.hpp"
+#include "typedefs.hpp"
+
+#endif // PRESENTATION_ALL_HPP
---
+++ src/presentation/allfwd.hpp
@@ -0,0 +1,36 @@
+// Distributes all types from this directory fwd declared.
+// Soultatos Stefanos 2022
+
+#ifndef PRESENTATION_ALLFWD_HPP
+#define PRESENTATION_ALLFWD_HPP
+
+namespace presentation
+{
+
+class graph_interface;
+class command_history;
+
+class weights_backend;
+class layout_backend;
+class scaling_backend;
+class degrees_backend;
+class clustering_backend;
+
+class background_renderer;
+class graph_renderer;
+class graph_collision_checker;
+
+class menu_bar;
+class weights_editor;
+class layout_editor;
+class scaling_editor;
+class degrees_editor;
+class clustering_editor;
+class controls_hud;
+class frames_hud;
+class overlay_manager;
+class popup_holder;
+
+} // namespace presentation
+
+#endif // PRESENTATION_ALLFWD_HPP
