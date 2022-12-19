d06262585ae162a3fcbe523a57dadceb2e1a00d6
SoultatosStefanos <stefanoss1498@gmail.com>
2022-12-19 14:50:33 +0200

    scaling commands integration tests

--- tests/presentation/commands_tests.cpp
+++ tests/presentation/commands_tests.cpp
@@ -255,4 +255,242 @@ TEST_F(layout_commands_tests, restore_layout_can_be_undone_and_redone)
     EXPECT_EQ(layout::get_scale(*backend), 1000);
 }
 
+/***********************************************************
+ * Scaling                                                 *
+ ***********************************************************/
+
+class scaling_commands_tests : public Test
+{
+protected:
+    void SetUp() override
+    {
+        cmds = std::make_unique< command_history >();
+        backend = std::make_unique< scaling_backend >(scaling_config {
+            { "f", scaling::make_x_factor(10, true, 0, 10) } });
+    }
+
+    std::unique_ptr< command_history > cmds;
+    std::unique_ptr< scaling_backend > backend;
+};
+
+TEST_F(scaling_commands_tests, update_dims_updates_the_backend_accordingly)
+{
+    using dims_t = scaling::factor::dims_type;
+
+    update_scaling_factor_dims(*cmds, *backend, "f", { true, true, true });
+
+    EXPECT_EQ(
+        scaling::get_factor_dims(*backend, "f"), dims_t({ true, true, true }));
+}
+
+TEST_F(scaling_commands_tests, update_dims_can_be_undone)
+{
+    using dims_t = scaling::factor::dims_type;
+
+    update_scaling_factor_dims(*cmds, *backend, "f", { true, true, true });
+
+    EXPECT_TRUE(cmds->can_undo());
+    cmds->undo();
+
+    EXPECT_EQ(
+        scaling::get_factor_dims(*backend, "f"),
+        dims_t({ true, false, false }));
+}
+
+TEST_F(scaling_commands_tests, update_dims_can_be_undone_and_redone)
+{
+    using dims_t = scaling::factor::dims_type;
+
+    update_scaling_factor_dims(*cmds, *backend, "f", { true, true, true });
+    cmds->undo();
+
+    EXPECT_TRUE(cmds->can_redo());
+    cmds->redo();
+
+    EXPECT_EQ(
+        scaling::get_factor_dims(*backend, "f"), dims_t({ true, true, true }));
+}
+
+TEST_F(scaling_commands_tests, update_baseline_updates_the_backend_accordingly)
+{
+    update_scaling_factor_baseline(*cmds, *backend, "f", 20);
+
+    EXPECT_EQ(scaling::get_factor_baseline(*backend, "f"), 20);
+}
+
+TEST_F(scaling_commands_tests, update_baseline_can_be_undone)
+{
+    update_scaling_factor_baseline(*cmds, *backend, "f", 20);
+
+    EXPECT_TRUE(cmds->can_undo());
+    cmds->undo();
+
+    EXPECT_EQ(scaling::get_factor_baseline(*backend, "f"), 10);
+}
+
+TEST_F(scaling_commands_tests, update_baseline_can_be_undone_and_redone)
+{
+    update_scaling_factor_baseline(*cmds, *backend, "f", 200);
+    cmds->undo();
+
+    EXPECT_TRUE(cmds->can_redo());
+    cmds->redo();
+
+    EXPECT_EQ(scaling::get_factor_baseline(*backend, "f"), 200);
+}
+
+TEST_F(
+    scaling_commands_tests,
+    update_enablement_updates_the_backend_accordingly)
+{
+    update_scaling_factor_enablement(*cmds, *backend, "f", false);
+
+    EXPECT_FALSE(scaling::is_factor_enabled(*backend, "f"));
+}
+
+TEST_F(scaling_commands_tests, update_enablement_can_be_undone)
+{
+    update_scaling_factor_enablement(*cmds, *backend, "f", false);
+
+    EXPECT_TRUE(cmds->can_undo());
+    cmds->undo();
+
+    EXPECT_TRUE(scaling::is_factor_enabled(*backend, "f"));
+}
+
+TEST_F(scaling_commands_tests, update_enablement_can_be_undone_and_redone)
+{
+    update_scaling_factor_enablement(*cmds, *backend, "f", false);
+    cmds->undo();
+
+    EXPECT_TRUE(cmds->can_redo());
+    cmds->redo();
+
+    EXPECT_FALSE(scaling::is_factor_enabled(*backend, "f"));
+}
+
+TEST_F(scaling_commands_tests, update_min_ratio_updates_the_backend_accordingly)
+{
+    update_scaling_factor_min_ratio(*cmds, *backend, "f", 0.5);
+
+    EXPECT_EQ(scaling::get_factor_min_ratio(*backend, "f"), 0.5);
+}
+
+TEST_F(scaling_commands_tests, update_min_ratio_can_be_undone)
+{
+    update_scaling_factor_min_ratio(*cmds, *backend, "f", 0.3);
+
+    EXPECT_TRUE(cmds->can_undo());
+    cmds->undo();
+
+    EXPECT_EQ(scaling::get_factor_min_ratio(*backend, "f"), 0);
+}
+
+TEST_F(scaling_commands_tests, update_min_ratio_can_be_undone_and_redone)
+{
+    update_scaling_factor_min_ratio(*cmds, *backend, "f", 0.25);
+    cmds->undo();
+
+    EXPECT_TRUE(cmds->can_redo());
+    cmds->redo();
+
+    EXPECT_EQ(scaling::get_factor_min_ratio(*backend, "f"), 0.25);
+}
+
+TEST_F(scaling_commands_tests, update_max_ratio_updates_the_backend_accordingly)
+{
+    update_scaling_factor_max_ratio(*cmds, *backend, "f", 5);
+
+    EXPECT_EQ(scaling::get_factor_max_ratio(*backend, "f"), 5);
+}
+
+TEST_F(scaling_commands_tests, update_max_ratio_can_be_undone)
+{
+    update_scaling_factor_max_ratio(*cmds, *backend, "f", 20);
+
+    EXPECT_TRUE(cmds->can_undo());
+    cmds->undo();
+
+    EXPECT_EQ(scaling::get_factor_max_ratio(*backend, "f"), 10);
+}
+
+TEST_F(scaling_commands_tests, update_max_ratio_can_be_undone_and_redone)
+{
+    update_scaling_factor_max_ratio(*cmds, *backend, "f", 30);
+    cmds->undo();
+
+    EXPECT_TRUE(cmds->can_redo());
+    cmds->redo();
+
+    EXPECT_EQ(scaling::get_factor_max_ratio(*backend, "f"), 30);
+}
+
+TEST_F(scaling_commands_tests, restore_scaling_updates_the_backend_accordingly)
+{
+    using dims_t = scaling::factor::dims_type;
+
+    update_scaling_factor_enablement(*cmds, *backend, "f", false);
+    update_scaling_factor_max_ratio(*cmds, *backend, "f", 5);
+    update_scaling_factor_min_ratio(*cmds, *backend, "f", 0.5);
+    update_scaling_factor_dims(*cmds, *backend, "f", { false, false, true });
+    update_scaling_factor_baseline(*cmds, *backend, "f", 200);
+
+    restore_scaling(*cmds, *backend);
+
+    EXPECT_TRUE(scaling::is_factor_enabled(*backend, "f"));
+    EXPECT_EQ(scaling::get_factor_max_ratio(*backend, "f"), 10);
+    EXPECT_EQ(scaling::get_factor_min_ratio(*backend, "f"), 0);
+    EXPECT_EQ(
+        scaling::get_factor_dims(*backend, "f"),
+        dims_t({ true, false, false }));
+    EXPECT_EQ(scaling::get_factor_baseline(*backend, "f"), 10);
+}
+
+TEST_F(scaling_commands_tests, restore_scaling_can_be_undone)
+{
+    using dims_t = scaling::factor::dims_type;
+
+    update_scaling_factor_enablement(*cmds, *backend, "f", false);
+    update_scaling_factor_max_ratio(*cmds, *backend, "f", 5);
+    update_scaling_factor_min_ratio(*cmds, *backend, "f", 0.5);
+    update_scaling_factor_dims(*cmds, *backend, "f", { false, false, true });
+    update_scaling_factor_baseline(*cmds, *backend, "f", 200);
+    restore_scaling(*cmds, *backend);
+
+    EXPECT_TRUE(cmds->can_undo());
+    cmds->undo();
+
+    EXPECT_FALSE(scaling::is_factor_enabled(*backend, "f"));
+    EXPECT_EQ(scaling::get_factor_max_ratio(*backend, "f"), 5);
+    EXPECT_EQ(scaling::get_factor_min_ratio(*backend, "f"), 0.5);
+    EXPECT_EQ(
+        scaling::get_factor_dims(*backend, "f"),
+        dims_t({ false, false, true }));
+    EXPECT_EQ(scaling::get_factor_baseline(*backend, "f"), 200);
+}
+
+TEST_F(scaling_commands_tests, restore_scaling_can_be_undone_and_redone)
+{
+    using dims_t = scaling::factor::dims_type;
+
+    update_scaling_factor_enablement(*cmds, *backend, "f", false);
+    update_scaling_factor_max_ratio(*cmds, *backend, "f", 5);
+    update_scaling_factor_min_ratio(*cmds, *backend, "f", 0.5);
+    update_scaling_factor_dims(*cmds, *backend, "f", { false, false, true });
+    update_scaling_factor_baseline(*cmds, *backend, "f", 200);
+    restore_scaling(*cmds, *backend);
+    cmds->undo();
+
+    EXPECT_TRUE(cmds->can_redo());
+    cmds->redo();
+
+    EXPECT_TRUE(scaling::is_factor_enabled(*backend, "f"));
+    EXPECT_EQ(scaling::get_factor_max_ratio(*backend, "f"), 10);
+    EXPECT_EQ(scaling::get_factor_min_ratio(*backend, "f"), 0);
+    EXPECT_EQ(
+        scaling::get_factor_dims(*backend, "f"),
+        dims_t({ true, false, false }));
+    EXPECT_EQ(scaling::get_factor_baseline(*backend, "f"), 10);
+}
+
 } // namespace
